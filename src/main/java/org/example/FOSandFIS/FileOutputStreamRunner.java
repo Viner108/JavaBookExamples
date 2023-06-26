6699cf507c283ac4f0186166cc681878262806e1
Viner108 <andrukovichl@mail.ru>
2023-06-26 17:45:32 +0300

    FIS and FOS commit

---
+++ src/main/java/org/example/FOSandFIS/FileOutputStreamRunner.java
@@ -0,0 +1,36 @@
+package org.example.FOSandFIS;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Path;
+
+public class FileOutputStreamRunner {
+    private static Path path=Path.of("test","Text.txt");
+    private static String text = "Hello Lera!\n"+"How are you?\n"+"yeyeyeyeyyeyeyyeyeyeyeyeyyyyeyeyeyeyeyyeyyeyeyeyeyeyeyeyeyeyeyeyeyeyeyeeyyeyeyeyeyyeyeyeyeyyyeyeyeyyyeyeyeyeyyeyeyeyy\n";
+    public static void main(String[] args) {
+        try(FileOutputStream fos=new FileOutputStream(path.toFile()))
+        {
+            byte[] buffer = text.getBytes();
+            for (int i = 0; i < text.length(); i++) {
+                fos.write(buffer[i]);
+            }
+            System.out.println("The file has been written");
+        }
+        catch(IOException ex){
+
+            System.out.println(ex.getMessage());
+        }
+        write();
+    }
+    public static void write(){
+        try (FileOutputStream fos=new FileOutputStream(path.toFile(),true)){
+            fos.write(text.getBytes());
+            fos.write("\n".getBytes());
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+}
